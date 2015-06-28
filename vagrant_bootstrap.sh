9e9197f332727e66a70bea54b5262f1432f11648
Tim Allen <flipper@peregrinesalon.com>
2015-06-28 16:51:15 -0400

    Modify vagrant user .bashrc file.

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Install git for version control, pip for install python packages
 echo 'Installing git and python3-pip...'
-sudo apt-get -y install git python3-pip > /dev/null
+sudo apt-get -qq install git python3-pip > /dev/null
 
 # Install virtualenv / virtualenvwrapper
 echo 'Installing virtualenv and virtualenvwrapper...'
-sudo pip3 install --quiet virtualenv virtualenvwrapper
-mkdir ~/.virtualenvs
-printf "\n\n#Virtualenv settings\n" >> ~/.bashrc
-printf "export WORKON_HOME=~/.virtualenvs\n" >> ~/.bashrc
-printf "export PROJECT_HOME=/vagrant\n" >> ~/.bashrc
-printf "export VIRTUALENVWRAPPER_PYTHON=python3\n" >> ~/.bashrc
-printf "source /usr/local/bin/virtualenvwrapper.sh\n" >> ~/.bashrc
+sudo pip3 install --quiet virtualenv
+sudo pip3 install --quiet virtualenvwrapper
+mkdir ~vagrant/.virtualenvs
+chown vagrant:vagrant ~vagrant/.virtualenvs
+printf "\n\n# Virtualenv settings\n" >> ~vagrant/.bashrc
+printf "export WORKON_HOME=~vagrant/.virtualenvs\n" >> ~vagrant/.bashrc
+printf "export PROJECT_HOME=/vagrant\n" >> ~vagrant/.bashrc
+printf "export VIRTUALENVWRAPPER_PYTHON=python3\n" >> ~vagrant/.bashrc
+printf "source /usr/local/bin/virtualenvwrapper.sh\n" >> ~vagrant/.bashrc
