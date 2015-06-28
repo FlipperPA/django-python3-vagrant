0bed66afeadfa7094f243e200a2634907bbdd1f3
Tim Allen <flipper@peregrinesalon.com>
2015-06-28 18:02:54 -0400

    Finish install script details with pathing.

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+
+# Use Python 3.4
+alias python='/usr/bin/python3.4'
+export PYTHONPATH=/usr/lib/python3.4
+
 # Install git for version control, pip for install python packages
 echo 'Installing git and python3-pip...'
 sudo apt-get -qq install git python3-pip > /dev/null
 
 # Install virtualenv / virtualenvwrapper
 echo 'Installing virtualenv and virtualenvwrapper...'
-sudo pip3 install --quiet virtualenv
-sudo pip3 install --quiet virtualenvwrapper
+alias python='/usr/bin/python3.4'
+pip3 install --quiet virtualenv
+pip3 install --quiet virtualenvwrapper
 mkdir ~vagrant/.virtualenvs
 chown vagrant:vagrant ~vagrant/.virtualenvs
 printf "\n\n# Virtualenv settings\n" >> ~vagrant/.bashrc
+printf "export PYTHONPATH=/usr/lib/python3.4" >> ~vagrant/.bashrc
 printf "export WORKON_HOME=~vagrant/.virtualenvs\n" >> ~vagrant/.bashrc
 printf "export PROJECT_HOME=/vagrant\n" >> ~vagrant/.bashrc
-printf "export VIRTUALENVWRAPPER_PYTHON=python3\n" >> ~vagrant/.bashrc
+printf "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.4\n" >> ~vagrant/.bashrc
 printf "source /usr/local/bin/virtualenvwrapper.sh\n" >> ~vagrant/.bashrc
 
 # Complete
